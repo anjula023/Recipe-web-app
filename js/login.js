fa3c41fcb7bb18761510f9a437fb1c25c9fc3c43
Anjula Kumari <kumarianjula71@gmail.com>
2024-04-14 22:19:45 +0530

    final touch

--- js/login.js
+++
@@ -1,6 +0,0 @@
-function login (){
-    let Email = document.getElementsByClassName("email").value;
-    let Name = document.getElementsByClassName("name").value;
-    let Username = document.getElementsByClassName("username").value;
-    let Password = document.getElementsByClassName("pswd").value;
-}
--- js/recipe.js
+++
@@ -1,6 +0,0 @@
-const follow =document.getElementById("follow");
-
-function change(event){
-    event.target.textContent="Following";
-}
-follow.addEventListener("click",change);
